a06670b6e9171e99e62e8c2a5e75a1658fe13f59
Bernard <twistor75@gmail.com>
2025-06-09 13:07:29 +0800

    Commit2

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -8,38 +8,39 @@ export default function TodoCard({ todo }) {
     const [timerInterval, setTimerInterval] = useState(null);
 
     const startTimer = () => {
-        const intervalID = setInterval(() => {
-            setTimer((prevTimer) => prevTimer + 1);
-        }, 1000);
-        setTimerInterval(intervalID);
-    };
-
-    const pauseTimer = () => {
-        clearInterval(null);
-        setTimerInterval(null);
-    };
-
-    useEffect(() => {
-        return () => {
-            clearInterval(timerInterval);
+        if (timerInterval === null) {
+            const intervalID = setInterval(() => {
+                setTimer((prevTimer) => prevTimer + 1);
+            }, 1000);
+            setTimerInterval(intervalID);
         };
-    }, [timerInterval]);
-    return (
-        <>
-            <Card border={border} className="my-3">
-                <Card.Header>{!completed && "Not"}Completed</Card.Header>
-                <Card.Body>
-                    <Card.Title>{todo.title}</Card.Title>
-                    <Card.Text>{todo.descriiption}</Card.Text>
-                    <p>Timer: {timer} seconds </p>
-                    <Button onClick={startTimer}>
-                        <i className="bi bi-play"></i>
-                    </Button>
-                    <Button onClick={pauseTimer} className="mx-2">
-                        <i className="bi bi-pause-fill"></i>
-                    </Button>
-                </Card.Body>
-            </Card>
-        </>
-    );
-}
+
+        const pauseTimer = () => {
+            clearInterval(null);
+            setTimerInterval(null);
+        };
+
+        useEffect(() => {
+            return () => {
+                clearInterval(timerInterval);
+            };
+        }, [timerInterval]);
+        return (
+            <>
+                <Card border={border} className="my-3">
+                    <Card.Header>{!completed && "Not"}Completed</Card.Header>
+                    <Card.Body>
+                        <Card.Title>{todo.title}</Card.Title>
+                        <Card.Text>{todo.descriiption}</Card.Text>
+                        <p>Timer: {timer} seconds </p>
+                        <Button onClick={startTimer}>
+                            <i className="bi bi-play"></i>
+                        </Button>
+                        <Button onClick={pauseTimer} className="mx-2">
+                            <i className="bi bi-pause-fill"></i>
+                        </Button>
+                    </Card.Body>
+                </Card>
+            </>
+        );
+    }
